6ad46bfcd1cb28195c6e22955ce058927b77bc74
Ivanmatthew <76107136+Ivanmatthew@users.noreply.github.com>
2022-11-19 16:17:19 +0000

    Deploying to gh-pages from @ Ivanmatthew/extensions-generic@68c9a6741536d65b867eaa6814b2f07ef024ef64 🚀

--- patch-1/Manga3asq/source.js
+++ patch-1/Manga3asq/source.js
@@ -20177,34 +20177,144 @@ exports.sourceSettings = sourceSettings;
 },{"random-useragent":58}],64:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SKScans = exports.SKScansInfo = void 0;
+exports.Manga3asq = exports.Manga3asqInfo = void 0;
 const paperback_extensions_common_1 = require("paperback-extensions-common");
 const Madara_1 = require("../Madara");
-const DOMAIN = 'https://skscans.com';
-exports.SKScansInfo = {
+const Manga3asqParser_1 = require("./Manga3asqParser");
+const DOMAIN = 'https://3asq.org';
+exports.Manga3asqInfo = {
     version: Madara_1.getExportVersion('0.0.0'),
-    name: 'SKScans',
+    name: '3asq Manga',
     description: `Extension that pulls manga from ${DOMAIN}`,
-    author: 'GameFuzzy',
-    authorWebsite: 'http://github.com/gamefuzzy',
+    author: 'xOnlyFadi',
+    authorWebsite: 'http://github.com/TheNetsky',
     icon: 'icon.png',
-    contentRating: paperback_extensions_common_1.ContentRating.EVERYONE,
+    contentRating: paperback_extensions_common_1.ContentRating.MATURE,
     websiteBaseURL: DOMAIN,
     sourceTags: [
         {
             text: 'Notifications',
             type: paperback_extensions_common_1.TagType.GREEN
+        },
+        {
+            text: 'Arabic',
+            type: paperback_extensions_common_1.TagType.GREY
         }
     ]
 };
-class SKScans extends Madara_1.Madara {
+class Manga3asq extends Madara_1.Madara {
     constructor() {
         super(...arguments);
         this.baseUrl = DOMAIN;
-        this.languageCode = paperback_extensions_common_1.LanguageCode.ENGLISH;
+        this.languageCode = 'Arabic';
+        this.hasAdvancedSearchPage = true;
+        this.alternativeChapterAjaxEndpoint = true;
+        this.sourceTraversalPathName = 'manga';
+        this.parser = new Manga3asqParser_1.Manga3asqParser();
     }
 }
-exports.SKScans = SKScans;
+exports.Manga3asq = Manga3asq;
 
-},{"../Madara":60,"paperback-extensions-common":14}]},{},[64])(64)
+},{"../Madara":60,"./Manga3asqParser":65,"paperback-extensions-common":14}],65:[function(require,module,exports){
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Manga3asqParser = void 0;
+const MadaraParser_1 = require("../MadaraParser");
+class Manga3asqParser extends MadaraParser_1.Parser {
+    constructor() {
+        super(...arguments);
+        this.parseDate = (date) => {
+            date = date.toUpperCase();
+            let time;
+            const number = Number((/\d*/.exec(date) ?? [])[0]);
+            if (date.includes('قبل ساعة') || date.includes('الان') || date.includes('ألان')) {
+                time = new Date(Date.now());
+            }
+            else if (date.includes('سنة') || date.includes('سنوات') || date.includes('سنوأت')) {
+                time = new Date(Date.now() - (number * 31556952000));
+            }
+            else if (date.includes('شهر') || date.includes('شهور')) {
+                time = new Date(Date.now() - (number * 2592000000));
+            }
+            else if (date.includes('اسبوع') || date.includes('اسوبيعن') || date.includes('اسابيع') || date.includes('أسبوع') || date.includes('أسابيع')) {
+                time = new Date(Date.now() - (number * 604800000));
+            }
+            else if (date.includes('أسوبيعن')) {
+                time = new Date(Date.now() - (2 * 604800000));
+            }
+            else if (date.includes('يومين')) {
+                time = new Date(Date.now() - (2 * 86400000));
+            }
+            else if (date.includes('يوم واحد') || date.includes('يوم وأحد')) {
+                time = new Date(Date.now() - (1 * 86400000));
+            }
+            else if (date.includes('يوم') || date.includes('ايام') || date.includes('أيام')) {
+                time = new Date(Date.now() - (number * 86400000));
+            }
+            else if (date.includes('ساعة') || date.includes('ساعه') || date.includes('ساعات')) {
+                time = new Date(Date.now() - (number * 3600000));
+            }
+            else if (date.includes('ساعة واحدة') || date.includes('ساعة وأحدة')) {
+                time = new Date(Date.now() - (1 * 3600000));
+            }
+            else if (date.includes('دقيقية') || date.includes('دقائق')) {
+                time = new Date(Date.now() - (number * 60000));
+            }
+            else if (date.includes('ثانية') || date.includes('ثانيا') || date.includes('ثواني') || date.includes('ثوأني')) {
+                time = new Date(Date.now() - (number * 1000));
+            }
+            else {
+                date = date.replace(/يناير/gi, 'January').replace(/فبراير/gi, 'February').replace(/مارس/gi, 'March').replace(/ابريل+|أبريل/gi, 'April').replace(/مايو/gi, 'May').replace(/يونيو/gi, 'June').replace(/يوليو/gi, 'July').replace(/أغسطس+|اغسطس/gi, 'August').replace(/سبتمبر/gi, 'September').replace(/أكتوبر+|اكتوبر/gi, 'October').replace(/نوفمبر/gi, 'November').replace(/ديسمبر/gi, 'December');
+                time = new Date(date);
+            }
+            return time;
+        };
+    }
+    async parseHomeSection($, source) {
+        const items = [];
+        for (const obj of $('div.page-item-detail').toArray()) {
+            const image = encodeURI(await this.getImageSrc($('img', obj), source) ?? '');
+            const title = $('a:not([target])', $('h3.h5', obj)).last().text();
+            const id = $('a:not([target])', $('h3.h5', obj)).attr('href')?.replace(`${source.baseUrl}/${source.sourceTraversalPathName}/`, '').replace(/\/$/, '');
+            const subtitle = $('span.font-meta.chapter', obj).first().text().trim();
+            if (!id || !title) {
+                console.log(`Failed to parse homepage sections for ${source.baseUrl}/`);
+                continue;
+            }
+            items.push(createMangaTile({
+                id: id,
+                image: image ? image : 'https://i.imgur.com/GYUxEX8.png',
+                title: createIconText({ text: this.decodeHTMLEntity(title) }),
+                subtitleText: createIconText({ text: this.decodeHTMLEntity(subtitle) })
+            }));
+        }
+        return items;
+    }
+    async parseSearchResults($, source) {
+        const results = [];
+        for (const obj of $(source.searchMangaSelector).toArray()) {
+            const id = ($('a:not([target])', obj).attr('href') ?? '').replace(`${source.baseUrl}/${source.sourceTraversalPathName}/`, '').replace(/\/$/, '');
+            const title = $('a:not([target])', obj).attr('title') ?? '';
+            const image = encodeURI(await this.getImageSrc($('img', obj), source));
+            const subtitle = $('span.font-meta.chapter', obj).text().trim();
+            if (!id || !title) {
+                if (id.includes(source.baseUrl.replace(/\/$/, '')))
+                    continue;
+                // Something went wrong with our parsing, return a detailed error
+                console.log(`Failed to parse searchResult for ${source.baseUrl} using ${source.searchMangaSelector} as a loop selector`);
+                continue;
+            }
+            results.push(createMangaTile({
+                id: id,
+                image: image ? image : 'https://i.imgur.com/GYUxEX8.png',
+                title: createIconText({ text: this.decodeHTMLEntity(title) }),
+                subtitleText: createIconText({ text: this.decodeHTMLEntity(subtitle) })
+            }));
+        }
+        return results;
+    }
+}
+exports.Manga3asqParser = Manga3asqParser;
+
+},{"../MadaraParser":62}]},{},[64])(64)
 });
