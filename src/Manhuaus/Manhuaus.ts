d0f39e4eef8cf3152583ed68e737290ed5c70c60
Ivan Matthew <76107136+Ivanmatthew@users.noreply.github.com>
2023-05-04 00:59:24 +0000

    create alternative manhuaus logic

--- src/Manhuaus/Manhuaus.ts
+++ src/Manhuaus/Manhuaus.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import {
     ContentRating,
     LanguageCode,
@@ -39,4 +43,42 @@ export class Manhuaus extends Madara {
     override chapterDetailsSelector = 'li.blocks-gallery-item > figure > img, div.page-break > img, div#chapter-video-frame > p > img, div.text-left > figure.wp-block-gallery > figure.wp-block-image > img, div.text-left > p > img'
     
     override alternativeChapterAjaxEndpoint = true
+
+    override constructAjaxHomepageRequest(page: number, postsPerPage: number, meta_key: string, meta_value?: string): any {
+        switch (meta_key) {
+            case '_latest_update':
+                return createRequestObject({
+                    url: `${this.baseUrl}/page/${page}`,
+                    method: 'GET'
+                })
+            case '_wp_manga_week_views_value':
+                break
+            case '_wp_manga_views':
+                break
+            case '_wp_manga_status':
+            default:
+                throw new Error('Invalid AJAX endpoint for homepage')
+        }
+        
+        return createRequestObject({
+            url: `${this.baseUrl}/wp-admin/admin-ajax.php`,
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+                'action': 'madara_load_more',
+                'template': 'madara-core/content/content-archive',
+                'page': page,
+                'vars[paged]': '1',
+                'vars[posts_per_page]': postsPerPage,
+                'vars[orderby]': 'meta_value_num',
+                'vars[sidebar]': 'right',
+                'vars[post_type]': 'wp-manga',
+                'vars[order]': 'desc',
+                'vars[meta_key]': meta_key,
+                'vars[meta_value]': meta_value
+            }
+        })
+    }
 }
